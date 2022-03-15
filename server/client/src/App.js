8858397ab09ad5a234a532529b1951d59c4e1fc6
Prashanthkv9871 <kvprashanth987@gmail.com>
2022-03-15 21:16:52 +0530

    Developer Hub

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter,Route,Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
-import Register from './components/Register.css/Register';
+import Register from './components/Register/Register';
 import Dashboard from './components/Dashboard/Dashboard';
 import Myprofile from './components/Myprofile/Myprofile';
 import Indprofile from './components/Indprofile/Indprofile';
